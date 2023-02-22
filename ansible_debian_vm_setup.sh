d5f3242f8370dc807cb110d86c532a55814ff1e3
Primusinterp <65064450+Primusinterp@users.noreply.github.com>
2023-02-22 11:27:08 +0100

    syntax errors fixed

--- ansible_debian_vm_setup.sh
+++ ansible_debian_vm_setup.sh
@@ -4,11 +4,13 @@ echo "[+] Setting up ansible VM"
 sudo apt install git -y
 git clone https://github.com/Orange-Cyberdefense/GOAD.git
 sudo apt install python3-pip -y
-python3 -m pip install ansible -y
+python3 -m pip install ansible 
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
 exec bash
-python3 -m pip install pywinrm -y
-ansible-galaxy install -r requirements.yml -y
-ansible-playbook -i ../ad/sevenkingdoms.local/inventory main.yml
+python3 -m pip install pywinrm 
+ansible-galaxy install -r requirements.yml 
 
-echo [+} setup complete"
+echo "[+} Setup complete"
+
+echo "cd into 'GOAD/ansible' and edit the inventory file.. then run:
+echo 'ansible-playbook -i ../ad/sevenkingdoms.local/inventory main.yml'
