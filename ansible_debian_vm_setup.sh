983fb99584e71932702b62e95c6e33698928c2fb
Primusinterp <65064450+Primusinterp@users.noreply.github.com>
2023-02-22 13:15:41 +0100

    updated for DCD

--- ansible_debian_vm_setup.sh
+++ ansible_debian_vm_setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "[+] Setting up ansible VM"
-sudo apt install git -y
-git clone https://github.com/Orange-Cyberdefense/GOAD.git
 sudo apt install python3-pip -y
 python3 -m pip install ansible 
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games"
 exec bash
 python3 -m pip install pywinrm 
+cd ~/ADSMGOAD/ansible
 ansible-galaxy install -r requirements.yml 
 
+echo "[+] Running ansible playbook
+ansible-playbook -i ~/ADSMGOAD/ansible/hosts main.yml
 echo "[+} Setup complete"
 
-echo "cd into 'GOAD/ansible' and edit the inventory file.. then run:
-echo 'ansible-playbook -i ../ad/sevenkingdoms.local/inventory main.yml'
+
