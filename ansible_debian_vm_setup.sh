3f8fb65e1cf776121671fc71cd176ce7a150e36f
Primusinterp <65064450+Primusinterp@users.noreply.github.com>
2023-02-23 11:22:19 +0100

    syntax fix

--- ansible_debian_vm_setup.sh
+++ ansible_debian_vm_setup.sh
@@ -2,15 +2,14 @@
 
 echo "[+] Setting up ansible VM"
 sudo apt install python3-pip -y
+sudo apt install sshpass
 python3 -m pip install ansible 
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/Oliver/.local/bin"
-exec bash
 python3 -m pip install pywinrm 
 cd ~/ADSMGOAD/ansible
 ansible-galaxy install -r requirements.yml 
+exec bash
 
-echo "[+] Running ansible playbook
-ansible-playbook -i ~/ADSMGOAD/ansible/hosts main.yml
 echo "[+} Setup complete"
 
 
