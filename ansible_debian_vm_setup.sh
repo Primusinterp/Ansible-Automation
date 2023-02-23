fcb2b5704a85dc72da446ae3640475e66b5204a9
Primusinterp <65064450+Primusinterp@users.noreply.github.com>
2023-02-23 12:04:41 +0100

    update script

--- ansible_debian_vm_setup.sh
+++ ansible_debian_vm_setup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 echo "[+] Setting up ansible VM"
+sudo apt update
 sudo apt install python3-pip -y
 sudo apt install sshpass
 python3 -m pip install ansible 
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/home/Oliver/.local/bin"
 python3 -m pip install pywinrm 
+exec bash
 cd ~/ADSMGOAD/ansible
 ansible-galaxy install -r requirements.yml 
-exec bash
 
 echo "[+} Setup complete"
 
